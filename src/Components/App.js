32b7ec5b7adb90a2642c600b76612efcff45af64
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-03-06 10:35:10 +0530

    small changes added

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <h1 className="text-xl h-[10vh] flex justify-center items-center text-[#5B8FB9] font-semibold">You can navigate through above links.</h1>
      { /* This element will render either <About/> when the url is "/about",<Contact/> when the url is "/contact" or null if it is "/" */}
       <Outlet />
       <div className="flex justify-around bg-[#EEEEEE] py-10">
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='py-10 h-[30vh] flex items-center justify-center'>
-      <h1 className='text-4xl text-[#5B8FB9] font-semibold'>This web page is about react router practice</h1>
+      <h1 className='text-4xl text-[#5B8FB9] font-semibold'>This web page is about react router.  </h1>
     </div>
   )
 }
